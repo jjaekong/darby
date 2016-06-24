63b825ecd515723cb2bb8cc8d3a1091a5c766d3b
김재홍 <jjaekong@gmail.com>
2016-06-24 19:28:36 +0900

    레이아웃 작업 완료

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>다비육종</title>
+    <link href="http://cdn.jsdelivr.net/font-nanum/1.0/nanumbarungothic/nanumbarungothic.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="/assets/css/default.css" rel="stylesheet">
+</head>
+<body>
+    <a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
+    <a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+    <header id="header">
+        <div class="container">
+            <h1 class="logo"><a href="/"><img src="/assets/images/logo.png" alt="다비육종"></a></h1>
+            <ul class="lang">
+                <li><a href="#">한국어</a></li>
+                <li><a href="#">ENGLISH</a></li>
+                <li><a href="#">VIETNAM</a></li>
+            </ul>
+            <ul class="help">
+                <li><a href="#">홈으로</a></li>
+                <li><a href="#">오시는 길</a></li>
+                <li><a href="#">인트라넷</a></li>
+            </ul>
+        </div>
+    </header>
+    <nav id="gnb" tabindex="-1">
+        <div class="wrapper">
+            <div class="container">
+                <ul>
+                    <li>
+                        <a href="#">회사소개</a>
+                        <ul>
+                            <li><a href="#">다비육종은</a></li>
+                            <li><a href="#">CEO</a></li>
+                            <li><a href="#">연혁</a></li>
+                            <li><a href="#">비전/CI</a></li>
+                            <li><a href="#">핵심 역량</a></li>
+                            <li><a href="#">오시는 길</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">사업소개</a>
+                        <ul>
+                            <li><a href="#">제품소개</a></li>
+                            <li><a href="#">유전사업</a></li>
+                            <li><a href="#">돈육사업</a></li>
+                            <li><a href="#">해외사업</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">홍보센터</a>
+                        <ul>
+                            <li><a href="#">보도자료</a></li>
+                            <li><a href="#">횽보자료</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">CSR</a>
+                        <ul>
+                            <li><a href="#">다비육종의 선행</a></li>
+                            <li><a href="#">활동내역</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">채용정보</a>
+                        <ul>
+                            <li><a href="#">채용공지</a></li>
+                            <li><a href="#">채용안내</a></li>
+                            <li><a href="#">인재상</a></li>
+                            <li><a href="#">복지제도</a></li>
+                            <li><a href="#">교육제도</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">오시는 길</a>
+                        <ul>
+                            <li><a href="#">오시는 길</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main id="content" tabindex="-1">
+        <div class="container">
+            #content
+        </div>
+    </main>
+    <nav id="nav-foot">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2">
+                    <h5>회사소개</h5>
+                    <ul class="menu">
+                        <li><a href="#">다비육종은</a></li>
+                        <li><a href="#">CEO</a></li>
+                        <li><a href="#">연혁</a></li>
+                        <li><a href="#">비전/CI</a></li>
+                        <li><a href="#">핵심 역량</a></li>
+                        <li><a href="#">오시는 길</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-2">
+                    <h5>사업소개</h5>
+                    <ul class="menu">
+                        <li><a href="#">제품소개</a></li>
+                        <li><a href="#">유전사업</a></li>
+                        <li><a href="#">돈육사업</a></li>
+                        <li><a href="#">해외사업</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-2">
+                    <h5>홍보센터</h5>
+                    <ul class="menu">
+                        <li><a href="#">보도자료</a></li>
+                        <li><a href="#">홍보자료</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-2">
+                    <h5>CSR</h5>
+                    <ul class="menu">
+                        <li><a href="#">다비육종의 선행</a></li>
+                        <li><a href="#">활동내역</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-2">
+                    <h5>채용</h5>
+                    <ul class="menu">
+                        <li><a href="#">채용공지</a></li>
+                        <li><a href="#">채용안내</a></li>
+                        <li><a href="#">인재상</a></li>
+                        <li><a href="#">복지제도</a></li>
+                        <li><a href="#">교육제도</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-2">
+                    <h5>다비육종 SNS</h5>
+                    <ul class="sns">
+                        <li class="fb"><a href="#">페이스북</a></li>
+                        <li class="pl"><a href="#">유투브</a></li>
+                        <li class="ks"><a href="#">카카오스토리</a></li>
+                    </ul>
+                    <div class="famiily dropup">
+                        <button class="btn btn-default btn-block dropdown-toggle" type="button" id="family-site" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            패밀리 사이트
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="family-site">
+                            <li><a href="#">사이트 #1</a></li>
+                            <li><a href="#">사이트 #2</a></li>
+                            <li><a href="#">사이트 #3</a></li>
+                            <li><a href="#">사이트 #4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-9">
+                    <ul class="contact">
+                        <li>
+                            <dl>
+                                <dt>본사</dt>
+                                <dd><address>경기도 안성시 일죽면 서동대로 7381-8</address></dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>전화번호</dt>
+                                <dd>031-672-5660</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>팩스</dt>
+                                <dd>031-672-5566</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                    <ul class="company">
+                        <li>
+                            <dl>
+                                <dt>대표</dt>
+                                <dd>윤희진/민동수</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>사업자등록번호</dt>
+                                <dd>125-81-15252</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                    <p class="copyright">COPYRIGHT (C) 2016 다비육종. ALL RIGHT RESERVED. <i>SITE BY <a href="http://www.movementk.com" target="_blank">MOVEMENT K GROUP</a>.</i></p>
+                </div>
+                <div class="col-xs-3">
+                    <ul class="cert">
+                        <li><img src="/assets/images/img_cert_1.png" alt="소시모"></li>
+                        <li><img src="/assets/images/img_cert_2.png" alt="식품안전관리인증"></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script>
+        (function($) {
+            $(document).on('mouseenter focusin', '#gnb .container', function() {
+                $('#gnb').addClass('opened');
+            });
+            $(document).on('mouseleave', '#gnb .container', function() {
+                $('#gnb').removeClass('opened');
+            }); 
+        })(jQuery);
+    </script>
+</body>
+</html>
