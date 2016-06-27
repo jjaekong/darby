98d894bce1567423b5bbddffd2c1da4eaa89eea7
김재홍 <jjaekong@gmail.com>
2016-06-27 16:41:44 +0900

    레이아웃 완료

---
+++ inc/header.php
@@ -0,0 +1,75 @@
+<a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
+<a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+<header id="header">
+    <div class="container">
+        <h1 class="logo"><a href="/"><img src="/assets/images/logo.png" alt="다비육종"></a></h1>
+        <ul class="lang">
+            <li><a href="#">한국어</a></li>
+            <li><a href="#">ENGLISH</a></li>
+            <li><a href="#">VIETNAM</a></li>
+        </ul>
+        <ul class="help">
+            <li><a href="#">홈으로</a></li>
+            <li><a href="#">오시는 길</a></li>
+            <li><a href="#">인트라넷</a></li>
+        </ul>
+    </div>
+</header>
+<nav id="gnb" tabindex="-1">
+    <div class="wrapper">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">회사소개</a>
+                    <ul>
+                        <li><a href="#">다비육종은</a></li>
+                        <li><a href="#">CEO</a></li>
+                        <li><a href="#">연혁</a></li>
+                        <li><a href="#">비전/CI</a></li>
+                        <li><a href="#">핵심 역량</a></li>
+                        <li><a href="#">오시는 길</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">사업소개</a>
+                    <ul>
+                        <li><a href="#">제품소개</a></li>
+                        <li><a href="#">유전사업</a></li>
+                        <li><a href="#">돈육사업</a></li>
+                        <li><a href="#">해외사업</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">홍보센터</a>
+                    <ul>
+                        <li><a href="#">보도자료</a></li>
+                        <li><a href="#">횽보자료</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">CSR</a>
+                    <ul>
+                        <li><a href="#">다비육종의 선행</a></li>
+                        <li><a href="#">활동내역</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">채용정보</a>
+                    <ul>
+                        <li><a href="#">채용공지</a></li>
+                        <li><a href="#">채용안내</a></li>
+                        <li><a href="#">인재상</a></li>
+                        <li><a href="#">복지제도</a></li>
+                        <li><a href="#">교육제도</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">오시는 길</a>
+                    <ul>
+                        <li><a href="#">오시는 길</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
