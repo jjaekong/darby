091e21f3950b7d0aedcb170b13e098bcb11ca4eb
kyh5392 <kyh90495392@gmail.com>
2016-06-27 19:23:56 +0900

    sub.php 작성 작업중 #1

---
+++ introduce/darby_introduce.php
@@ -0,0 +1,45 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet" />
+<link href="/assets/css/introduce.css" rel="stylesheet" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="page-header page-header-1">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">회사소개</a></li>
+				<li class="active">다비육종은</li>
+			</ol>
+			<hr>
+			<h1>다비육종은</h1>
+			<p>다비육종을 소개합니다.</p>
+		</div>
+	</div>
+	<main id="content" tabindex="-1" class="introduce darby-intro">
+		<div class="container">
+			<section class="intro">
+				<div class="section-header">
+					<h2><img src="/assets/images/introduce/intro_subject.jpg" alt="한국 양돈의 역사, 여기서 시작된다!"></h2>
+					<p>
+						다비육종은 1983년 설립 이래로<br>
+						한국 양돈산업의 발전을 선도해온 기업으로서<br>
+						그 행보 하나 하나가 한국돼지의 표준을 제시하여 왔으며,<br>
+						미래 양돈 사업의 영원한 동반자로 고객 여러분의 기억에 자리하고 있습니다.
+					</p>
+				</div>
+				<div class="section-content">
+					<p>
+						<i>다비육종은 국내 최대의 전문돼지육종기업</i>으로 우수한 유전자를 개량·보급, NEW·SEW 와 같은 발전된 양돈생산시스템을 도입, GP농장 계약생산체계 적용으로 새로운 양돈경영의 방법을 실현, 인공수정 (A.I)의 도입·보급으로 차별화된 번식 기술과 생산효율성을 제고, 우리 나라에서는 아무도 그 가능성을 눈여겨보지 않던 농장단위의 HACCP 인증과 적용으로 안전축산물 생산의 전(全) 단계 완성의 계기를 마련, 웰팜포크와 같은 맛있는돈육 생산을 위한 새로운 품종과 생산시스템을 개발, 아시아 시장에 종돈을 포함한 유전자원을 수출, 분단 이후로 북한에 종돈을 분양하는 등 <i>다비육종에는 수많은 국내최초라는 도전의 꼬리표가 붙어 왔습니다.</i>
+					</p>
+					<p>
+						다비육종은 <i>최고 품질의 종자와 기술을 보급하여 양돈산업의 발전을 선도</i>하고, 안전하고 맛있는 돈육을 개발하여 풍요로운 식생활 문화에 기여한다는 비전으로 <i>한국 양돈발전의 새 역사를 쓰기위한 도전을 결코 멈추지 않을 것입니다.</i>
+					</p>
+				</div>
+			</section>
+		</div>
+	</main>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
--- sub.php
+++ sub.php
@@ -1,4 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet" />
 </head>
 <body>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
@@ -14,11 +15,43 @@
             <p>다비육종을 소개합니다.</p>
         </div>
     </div>
-    <main id="content" tabindex="-1">
+    <main id="content" tabindex="-1" class="sub">
         <div class="container">
-            #content
+            <section>
+                <div class="section-header">
+                    <h3>복지제도</h3>
+                    <hr>
+                </div>
+                <div class="section-content">
+                    <ul class="dot-list">
+                        <li>주 40시간 근무제 실시</li>
+                        <li>장기근속자 포상금픔 지급 및 국내 또는 해외여행기회부여 (부부동반)</li>
+                        <li>본인 및 배우자의 매년 정기적인 건강검진실시 (보험수가 이외의 추가 검진항목 설계)</li>
+                        <li>본인생일 및 자녀출산시 축하금-품 제공</li>
+                        <li>퇴직자 기념금품 제공</li>
+                    </ul>
+					<ul class="arrow-list" style="margin:30px 0;">
+						<li>중부고속국도 일죽IC 진입</li>
+						<li>삼거리에서 좌회전</li>
+						<li>장호원, 충주 방향으로 1.7km 주행</li>
+						<li>일죽IC타운 직전 교통신호에서 U턴</li>
+						<li>현대오일뱅크 끼고 우회전</li>
+						<li>도착</li>
+					</ul>
+                </div>
+            </section>
         </div>
     </main>
+<style type="text/css">
+.sub .container section .section-content .arrow-list li {
+	display:inline-block;
+	font-size:16px; 
+	color:#4e4e4e;
+}
+.sub .container section .section-content .arrow-list li:after {
+	content:url(///)
+}
+</style>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
 </body>
