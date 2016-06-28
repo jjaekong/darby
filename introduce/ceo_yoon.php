8b80eee427a54470f5ee3728231475a95ab09aeb
kyh5392 <kyh90495392@gmail.com>
2016-06-28 14:58:53 +0900

    서브페이지 완료 #1 , 회사소개 > 다비육종은, 인사말, 완료 #1

---
+++ introduce/ceo_yoon.php
@@ -0,0 +1,42 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet" />
+<link href="/assets/css/introduce.css" rel="stylesheet" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="page-header page-header-1">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">회사소개</a></li>
+				<li class="active">인사말</li>
+			</ol>
+			<hr>
+			<h1>인사말</h1>
+			<p>다비육종을 찾아주신 여러분 안녕하세요.</p>
+		</div>
+	</div>
+	<main id="content" tabindex="-1" class="introduce greeting">
+		<div class="container">
+			<nav class="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="/introduce/greeting.php">인사말</a></li>
+					<li><a href="/introduce/ceo_yoon.php">CEO 윤희진</a></li>
+					<li class="active"><a href="/introduce/ceo_min.php">CEO 민동수</a></li>
+				</ul>
+			</nav>
+			<section class="intro">
+				<div class="section-header">
+					<h2>"여러분의 내일에도 <br><i>다비육종이 든든한 동행이 되어드리겠습니다."</i></h2>
+				</div>
+				<div class="section-content">
+					
+				</div>
+				<h3><small>대표이사</small>윤희진, 민동수</h3>
+			</section>
+		</div>
+	</main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ introduce/greeting.php
@@ -0,0 +1,68 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet" />
+<link href="/assets/css/introduce.css" rel="stylesheet" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="page-header page-header-1">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">회사소개</a></li>
+				<li class="active">인사말</li>
+			</ol>
+			<hr>
+			<h1>인사말</h1>
+			<p>다비육종을 찾아주신 여러분 안녕하세요.</p>
+		</div>
+	</div>
+	<main id="content" tabindex="-1" class="introduce greeting">
+		<div class="container">
+			<nav class="navbar">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="/introduce/greeting.php">인사말</a></li>
+					<li><a href="/introduce/ceo_yoon.php">CEO 윤희진</a></li>
+					<li><a href="/introduce/ceo_min.php">CEO 민동수</a></li>
+				</ul>
+			</nav>
+			<section class="intro">
+				<div class="section-header">
+					<h2>"여러분의 내일에도 <br><i>다비육종이 든든한 동행이 되어드리겠습니다."</i></h2>
+				</div>
+				<div class="section-content">
+					<div class="greeting-text">
+						<p>
+							다비육종은 1983년, 당시 우리나라 양돈이 이제 막 기업화, 계열화를 통한 규모나 경영방식 면에서의 구조변화를 모색하던 시기에, 좀 더 멀리 내다보고 즉, 국내 양돈의 생산력 증대를 통한 보다 안전하고 맛 좋은 돼지고기를 저렴하게 생산할 수 있도록 우리나라에서도 독자적인 종자사업을 해야겠다는 목적으로 설립되었습니다.  
+						</p>
+						<p>
+							다비육종은 그간 이러한 설립취지에 맞게 눈앞의 이익만을 쫓지 않고 우수한 유전자원과 새로운 양돈기술 개발에 투자와 노력을 아끼지 않은 덕에 <i>A.I.의 실용화, HACCP의 도입 등 우리나라 양돈업의 경쟁력을 높이려는 새롭고 다양한 시도들을 현실화</i> 할 수 있었습니다.
+						</p>
+						<p>
+							농업은 인간과 자연을 이어주는 생태환경 에너지 산업입니다. 그중에서도 특히 양돈은 이런 에너지의 균형이 깨지지 않도록 그 운영에 지혜를 담아야만 성공하는 사업입니다. 이제 우리나라 양돈 산업은 그간의 양적 성장을 넘어 친환경, 유기축산, 동물복지와 같은 질적 발전을 함께 이루어야 하는 패러다임의 전환점에 와 있습니다. 인간의 건강한 삶과 지구환경과의 조화를 염두에 둔 성장과 발전만이 산업동물을 근간으로 하는 기업의 존재에 의미를 부여받게 되는 셈인 것이죠. 
+						</p>
+					</div>
+					<div class="greeting-text">
+						<p>
+							준비하는 자 만이 기회를 살린다는 말이 있습니다. <br>
+							쉽지만은 않은 주변 경영여건의 변화와 고객의 다양한 욕구에 부응할 수 있도록 안전한 고품질의 축산물 생산과 효율적인 자원 활용, 환경을 보호하는 신기술을 개발과 미래의 양돈 산업을 이끌어갈 인재를 육성하는 등 지금껏 그래왔듯이 새로운 고객가치를 창출하는 기업, 다비육종이 누구보다 한 발짝 앞장서 뛰겠습니다.
+						</p>
+						<p>
+							어제와 오늘, 다비육종이 일궈 온 도전과 발전의 역사는 고객과 주주 그리고 다비육종을 믿고 어려운 길을 함께 걷기를 마다치 않은 협력사 여러분이 있었기에 가능한 것이었습니다. 다비육종은 지금까지의 성과에 만족하지 않고 <i>국내 양돈업의 경쟁력을 확보하고 새로운 성장 동력을 발굴, 나아가 아시아를 위시한 세계시장을 개척</i>하여 우리나라 양돈 산업의 위상을 드높이는데 밑거름이 되고자 더욱 노력하겠습니다.
+						</p>
+						<p>
+							다시 한 번 다비육종의 홈페이지를 찾아주신 여러분께 감사드리며, <i>한결같은 믿음을 주는 오랜 친구처럼 여러분의 내일에도 다비육종이 든든한 동행이 되어드리겠습니다.</i>
+						</p>
+
+						<p>
+							감사합니다.
+						</p>
+					</div>
+				</div>
+				<h3><small>대표이사</small>윤희진, 민동수</h3>
+			</section>
+		</div>
+	</main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
