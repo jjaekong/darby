9c7375ae27b62082b7ff10b2cdb514f8ac921c40
kyh5392 <kyh90495392@gmail.com>
2016-06-30 12:53:23 +0900

    회사소개 > 오시는 길 #1 완료.

---
+++ introduce/location.php
@@ -0,0 +1,75 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet" />
+<link href="/assets/css/introduce.css" rel="stylesheet" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="page-header page-header-1">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">회사소개</a></li>
+				<li class="active">오시는 길</li>
+			</ol>
+			<hr>
+			<h1>오시는 길</h1>
+			<p>다비육종을 찾아오시는 길 입니다.</p>
+		</div>
+	</div>
+	<main id="content" tabindex="-1" class="introduce location">
+		<div class="container">
+			<section class="map">
+				<div class="section-header">
+					<h3>약도</h3>
+					<hr>
+				</div>
+				<p>
+					<img src="/assets/images/introduce/map.jpg" alt="약도">
+				</p>
+			</section>
+			<section class="company">
+				<div class="section-header">
+					<h3>다비육종 본사</h3>
+					<hr>
+				</div>
+				<ul>
+					<li>
+						<dl>
+							<dt>주소</dt>
+							<dd>경기도 안성시 일죽면 서동대로 7381-8    (우) 17529</dd>
+						</dl>
+					</li>
+					<li>
+						<dl>
+							<dt>TEL</dt>
+							<dd>031-672-5900</dd>
+						</dl>
+					</li>
+					<li>
+						<dl>
+							<dt>FAX</dt>
+							<dd>031-672-5566</dd>
+						</dl>
+					</li>
+				</ul>
+			</section>
+			<section class="location-info">
+				<div class="section-header">
+					<h3>오시는길</h3>
+					<hr>
+				</div>
+				<ul class="arrow-list">
+					<li>중부고속국도 일죽IC 진입</li>
+					<li>삼거리에서 좌회전</li>
+					<li>장호원, 충주 방향으로 1.7km 주행</li>
+					<li>일죽IC타운 직전 교통신호에서 U턴</li>
+					<li>현대오일뱅크 끼고 우회전</li>
+					<li>도착</li>
+				</ul>
+			</section>
+		</div>
+	</main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
